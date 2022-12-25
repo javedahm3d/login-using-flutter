a6a09e7969e794a19549e789703789360957b3c2
Javed <chaudharyjavedahmed@gmail.com>
2022-12-25 13:18:36 +0530

    login ui

---
+++ android/app/src/main/kotlin/com/example/campus/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.campus
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/pages/auth_page.dart
@@ -0,0 +1,28 @@
+import 'package:campus/pages/homePage.dart';
+import 'package:campus/pages/login_page.dart';
+import 'package:flutter/material.dart';
+
+
+class AuthPage extends StatelessWidget {
+  const AuthPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      // body: StreamBuilder<User?>(
+      //   stream: FirebaseAuth.instance.authStateChanges(),
+      //   builder: (context, snapshot) {
+      //     //is user logged in
+      //     if (snapshot.hasData) {
+      //      return  HomePage();
+      //     }
+
+      //     //is user not logged in
+      //     else {
+      //       return LoginPage();
+      //     }
+      //   },
+      // ),
+    );
+  }
+}
