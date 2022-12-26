c4f2a747b3f1d9f31eda2683329aad2dfe4c81e0
Javed <chaudharyjavedahmed@gmail.com>
2022-12-26 15:39:22 +0530

    added forgot password functionality

--- lib/pages/homePage.dart
+++ lib/pages/homePage.dart
@@ -1,15 +1,16 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
-
-
+import 'package:google_sign_in/google_sign_in.dart';
 
 class HomePage extends StatelessWidget {
-   HomePage({super.key});
+  HomePage({super.key});
 
-  void signUserOut() {
-  FirebaseAuth.instance.signOut();
-}
+  void signUserOut() async {
+    final GoogleSignIn googleSignIn = GoogleSignIn();
+    await FirebaseAuth.instance.signOut();
+    await googleSignIn.signOut();
+  }
 
   final user = FirebaseAuth.instance.currentUser!;
 
@@ -24,8 +25,7 @@ class HomePage extends StatelessWidget {
           )
         ],
       ),
-      body: Center(
-        child: Text("logged in as "+ user.email!)),
+      body: Center(child: Text("logged in as " + user.email!)),
     );
   }
 }
--- lib/services/auth_service.dart
+++ lib/services/auth_service.dart
@@ -1,7 +1,8 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:google_sign_in/google_sign_in.dart';
+import 'package:flutter/material.dart';
 
-class AuthService {
+class AuthService extends ChangeNotifier {
   signInWithGoogle() async {
     //google accounts options
     final GoogleSignInAccount? gUser = await GoogleSignIn().signIn();
@@ -17,4 +18,6 @@ class AuthService {
 
     return await FirebaseAuth.instance.signInWithCredential(credential);
   }
+
+  
 }
